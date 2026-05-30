65212b65cd01e516d8eec604e25b09cbdda45a29
AndN0tOr <tranhuytuan.vietnam@gmail.com>
2026-05-30 15:34:19 +0700

    No commit message.

---
+++ headers/main.h
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<time.h>
+
+#include "../headers/bitmaps.h"
+#include "../headers/JPEG_Encode.h"
+#include "../headers/JPEG_steps.h"
+#include "../headers/pixel_MCU_image.h"
+#include "../headers/test.h"
